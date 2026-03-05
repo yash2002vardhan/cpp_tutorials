da049a8487b397909994e0aaf8ca82287762abad
Yashvardhan Goel <yash2002goel@gmail.com>
2026-03-06 00:55:36 +0530

    refactor: add example of multiple inheritance with MyClass and MyOtherClass in inheritance.cpp

--- inheritance.cpp
+++ inheritance.cpp
@@ -1,29 +1,58 @@
 #include <iostream>
 using namespace std;
 
-class Vehicle{
+// class Vehicle{
+//     public:
+//     string brand = "Ford";
+
+//     void honk(){
+//         cout << "Tutt, tutt!";
+//     };
+// };
+
+// class Car : public Vehicle{
+//     public:
+//     string model = "Mustang";
+// };
+
+
+
+// int main() {
+
+//     Car myCar;
+
+//     cout << myCar.brand;
+//     myCar.honk();
+//     cout << myCar.brand;
+
+//     return 0;
+// }
+
+
+// multiple inheritance
+
+class MyClass {
     public:
-    string brand = "Ford";
-
-    void honk(){
-        cout << "Tutt, tutt!";
-    };
-};
-
-class Car : public Vehicle{
+      void myFunction() {
+        cout << "Some content in parent class." << "\n" ;
+      }
+  };
+  
+  // Another base class
+  class MyOtherClass {
     public:
-    string model = "Mustang";
-};
-
-
-
-int main() {
-
-    Car myCar;
-
-    cout << myCar.brand;
-    myCar.honk();
-    cout << myCar.brand;
-
+      void myOtherFunction() {
+        cout << "Some content in another class." ;
+      }
+  };
+  
+  // Derived class
+  class MyChildClass: public MyClass, public MyOtherClass {
+  };
+  
+  int main() {
+    MyChildClass myObj;
+    myObj.myFunction();
+    myObj.myOtherFunction();
     return 0;
-}
+  }
