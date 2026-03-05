cf854bb0c912cd21a9adbc6ac7be7eaad95271fd
Yashvardhan Goel <yash2002goel@gmail.com>
2026-03-06 01:02:57 +0530

    refactor: restructure inheritance example by implementing Employee and Programmer classes with protected access specifier in inheritance.cpp

--- inheritance.cpp
+++ inheritance.cpp
@@ -31,28 +31,58 @@ using namespace std;
 
 // multiple inheritance
 
-class MyClass {
-    public:
-      void myFunction() {
-        cout << "Some content in parent class." << "\n" ;
-      }
-  };
+// class MyClass {
+//     public:
+//       void myFunction() {
+//         cout << "Some content in parent class." << "\n" ;
+//       }
+//   };
   
-  // Another base class
-  class MyOtherClass {
-    public:
-      void myOtherFunction() {
-        cout << "Some content in another class." ;
-      }
+//   // Another base class
+//   class MyOtherClass {
+//     public:
+//       void myOtherFunction() {
+//         cout << "Some content in another class." ;
+//       }
+//   };
+  
+//   // Derived class
+//   class MyChildClass: public MyClass, public MyOtherClass {
+//   };
+  
+//   int main() {
+//     MyChildClass myObj;
+//     myObj.myFunction();
+//     myObj.myOtherFunction();
+//     return 0;
+//   }
+
+
+// 'protected' access specifier
+
+
+class Employee {
+    protected: // Protected access specifier
+      int salary;
   };
   
   // Derived class
-  class MyChildClass: public MyClass, public MyOtherClass {
+  class Programmer: public Employee {
+    public:
+      int bonus;
+      void setSalary(int s) {
+        salary = s;
+      }
+      int getSalary() {
+        return salary;
+      }
   };
   
   int main() {
-    MyChildClass myObj;
-    myObj.myFunction();
-    myObj.myOtherFunction();
+    Programmer myObj;
+    myObj.setSalary(50000);
+    myObj.bonus = 15000;
+    cout << "Salary: " << myObj.getSalary() << "\n";
+    cout << "Bonus: " << myObj.bonus << "\n";
     return 0;
   }
